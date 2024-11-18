205492809e662d776e8f7e3496a58d99da2de1d3
Anirudha Bele <beleanirudha8@gmail.com>
2024-11-18 17:18:02 +0530

    add book data

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.json());
 
+const books = [
+	{ id: 1, title: "Book 1", author: "Author 1" },
+	{ id: 2, title: "Book 2", author: "Author 2" },
+	{ id: 3, title: "Book 3", author: "Author 3" },
+];
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = 8080;
