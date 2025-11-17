8facbf3cdcefd659e488505785de8b9183b4cd48
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-17 08:29:11 +0530

    add bookd data

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+const books = [
+	{ id: 1, title: "Book 1", author: "Author 1" },
+	{ id: 2, title: "Book 2", author: "Author 2" },
+	{ id: 3, title: "Book 3", author: "Author 3" },
+];
+
 const port = 8080;
 
 app.listen(port, () => {
